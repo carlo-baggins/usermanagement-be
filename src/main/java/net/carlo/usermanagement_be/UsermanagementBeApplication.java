3aebe8744f05691cd25471a3d858e2a657bbc88c
carlo <carlo.miglierina@gmail.com>
2026-01-16 15:47:28 +0100

    creazione progetto

---
+++ src/main/java/net/carlo/usermanagement_be/UsermanagementBeApplication.java
@@ -0,0 +1,13 @@
+package net.carlo.usermanagement_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/carlo/usermanagement_be/UsermanagementBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.carlo.usermanagement_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
